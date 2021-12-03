ce35d511b55d13aea8dccadd63c4945b6191dd34
Folahanmi Kolawole <fola@aeeiee.com>
2021-12-03 01:47:30 +0100

    create inital tabbar controller with corresponding view controllers embedded in

---
+++ AutoChek/AutoChek/Controllers/MessagesVC.swift
@@ -0,0 +1,16 @@
+//
+//  MessagesVC.swift
+//  AutoChek
+//
+//  Created by FOLAHANMI KOLAWOLE on 03/12/2021.
+//
+
+import UIKit
+
+class MessagesVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+  }
+}
